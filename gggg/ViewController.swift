d98bf37e176f81802fa6b2833b5c3ecb14f178b5
MaxLaurel <max.filippov.3333@gmail.com>
2023-03-03 15:11:29 +0300

    first commit for gggg

---
+++ gggg/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  gggg
+//
+//  Created by Максим on 30.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var colorLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(changeColor), name: Notification.Name("red"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(changeColor2), name: Notification.Name("green"), object: nil)
+    }
+    @objc func changeColor() {
+            colorLabel.backgroundColor = UIColor.red
+        navigationController?.popViewController(animated: true)
+        }
+    @objc func changeColor2() {
+            colorLabel.backgroundColor = UIColor.green
+        navigationController?.popViewController(animated: true)
+    }
+
+    }
+
---
+++ secondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  secondViewController.swift
+//  gggg
+//
+//  Created by Максим on 30.09.2022.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func redButton(_ sender: Any) {
+        NotificationCenter.default.post(name: Notification.Name("red"), object: nil)
+    }
+    
+    @IBAction func greenButton(_ sender: Any) {
+        NotificationCenter.default.post(name:Notification.Name("green"), object: nil)
+    }
+}
+
